fce1aa89bef90e59bf4cbf54e2815cb0028cc488
Ricardo Figueiredo <ricardocastrofigueiredo@gmail.com>
2025-01-22 18:57:12 +0000

    dynamically display cards in hand

--- public/pages/game/script.js
+++ public/pages/game/script.js
@@ -4,66 +4,86 @@ const addBorderToDivs = () => {
   const divs = document.querySelectorAll("div");
   divs.forEach((div) => {
     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    console.log(randomColor);
     div.style.border = `1px solid ${randomColor}`;
   });
 };
 
-const createRandomCards = async (amount) => {
-  const handContainer = document.getElementById("player-container").querySelector(".hand-container");
-  for (let i = 0; i < amount; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("unit-card-vertical-component");
-    handContainer.appendChild(newDiv);
-  }
-  const cardContainers = document.getElementsByClassName("unit-card-vertical-component");
-  const traitCodes = [
-    "barrier",
-    "bloodthirsty",
-    "burned",
-    "creator",
-    "cursed",
-    "dealer",
-    "doomed",
-    "exhausted",
-    "ghost",
-    "heavy",
-    "immune",
-    "lastonestanding",
-    "lethal",
-    "pierce",
-    "poisoned",
-    "reflect",
-    "regenerate",
-    "resilient",
-    "rooted",
-    "ruthless",
-    "sharpshooter",
-    "strong",
-    "stunned",
-    "taunt",
-    "weak",
-  ];
-  for (let i = 0; i < cardContainers.length; i++) {
-    const container = cardContainers[i];
-    // randomize the card info
-    const randomId = Math.floor(Math.random() * 6);
-    const randomCodeNumber = Math.floor(Math.random() * 16);
-    const shuffledTraitCodes = traitCodes.sort(() => 0.5 - Math.random());
-    const selectedTraitCodes = shuffledTraitCodes.slice(0, randomCodeNumber);
-    // load card component
-    await loadComponent(container, "unit-card-vertical", {
-      id: randomId,
-      traitCodes: selectedTraitCodes,
-      placedPosition: null,
-      currentHp: null,
-      isSmall: true,
-    });
-  }
+const loadHand = async () => {
+  const handContainers = document.querySelectorAll(".hand-container");
+  const handContainerWidth = handContainers[0].offsetWidth;
+  const createRandomCards = async (amount, hand) => {
+    const traitCodes = [
+      "barrier",
+      "bloodthirsty",
+      "burned",
+      "creator",
+      "cursed",
+      "dealer",
+      "doomed",
+      "exhausted",
+      "ghost",
+      "heavy",
+      "immune",
+      "lastonestanding",
+      "lethal",
+      "pierce",
+      "poisoned",
+      "reflect",
+      "regenerate",
+      "resilient",
+      "rooted",
+      "ruthless",
+      "sharpshooter",
+      "strong",
+      "stunned",
+      "taunt",
+      "weak",
+    ];
+    for (let i = 0; i < amount; i++) {
+      // create div
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("unit-card-vertical-component");
+      handContainers[hand].appendChild(newDiv);
+      // randomize the card info
+      const randomId = Math.floor(Math.random() * 6);
+      const randomCodeNumber = Math.floor(Math.random() * 16);
+      const shuffledTraitCodes = traitCodes.sort(() => 0.5 - Math.random());
+      const selectedTraitCodes = shuffledTraitCodes.slice(0, randomCodeNumber);
+      // load card component
+      await loadComponent(newDiv, "unit-card-vertical", {
+        id: randomId,
+        traitCodes: selectedTraitCodes,
+        placedPosition: null,
+        currentHp: null,
+        isSmall: true,
+      });
+    }
+  };
+  // debugging
+  const deb = 4;
+  await createRandomCards(deb, 0);
+  await createRandomCards(deb + 1, 1);
+
+  // offset cards
+  handContainers.forEach((hand) => {
+    const cards = hand.querySelectorAll(".unit-card-vertical-component");
+    if (cards.length === 0) return;
+    const cardWidth = cards[0].offsetWidth;
+    if (cards.length * cardWidth < handContainerWidth) {
+      hand.style.justifyContent = "center";
+    } else {
+      const cardOffset = (handContainerWidth - cardWidth) / (cards.length - 1);
+      cards.forEach((card, index) => {
+        if (index !== 0) card.style.marginLeft = `${-cardWidth + cardOffset}px`;
+      });
+    }
+  });
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
   // debugging
   addBorderToDivs();
-  await createRandomCards(3);
+
+  // hand
+  await loadHand();
 });
